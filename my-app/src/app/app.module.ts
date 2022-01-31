7964f590f56978d7df9d517c3a421b1bab688f34
StepinaHelen <stopinahelen@gmail.com>
2022-01-31 23:17:56 +0200

    lesson 1

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarsFormComponent } from './cars-form/cars-form.component';
+import { CarComponent } from './car/car.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarsFormComponent,
+    CarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
